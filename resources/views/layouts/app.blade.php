701456c80ed46d484e96a08638ca19c9a4052f5f
Latik <latik@bistracker.org.ua>
2016-05-26 14:52:06 +0300

    use CDN for bootstrap and jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Мониторинг cтатусов портов</title>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <title>Мониторинг cтатусов магистральных портов(downlinks)</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link href="/css/main.css" rel="stylesheet">
 </head>
 <body id="app-layout">
@@ -32,7 +32,9 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="/js/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="/js/main.js"></script>
     @yield('scripts')
 
 </body>
