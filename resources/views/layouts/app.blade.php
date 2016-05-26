4759203b3e5b00fa7df6aab7d2af232fda8f36b4
Latik <latik@bistracker.org.ua>
2016-05-26 15:30:25 +0300

    rename title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
-                <a class="navbar-brand" href="/">Мониторинг портов коммутатора на статус линка</a>
+                <a class="navbar-brand" href="/">Мониторинг портов</a>
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
