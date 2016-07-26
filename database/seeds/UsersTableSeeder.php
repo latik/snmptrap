64848423b46da61491a3c59a5e666edd4c990f34
Latik <latik@bistracker.org.ua>
2016-07-27 01:19:58 +0300

    seed admin user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+            'name'     => 'Admin',
+            'email'    => 'admin@example.net',
+            'password' => bcrypt('admin'),
+        ]);
+    }
+}
