027aec574ccf170d0fc315ca31e0a5e6d2fca1a2
Latik <latik@bistracker.org.ua>
2016-08-03 09:56:30 +0300

    add some trap mibs

--- app/Console/Commands/TrapHandler.php
+++ app/Console/Commands/TrapHandler.php
@@ -50,8 +50,21 @@ class TrapHandler extends Command
             case 'IF-MIB::linkDown':
                 Event::fire(new LinkChangeTrap($trap));
                 break;
-            case 'SNMPv2-MIB::authenticationFailure':
+            //               .1.3.6.1.4.1.259.8.1.11.2.1.0.1   swPowerStatusChangeTrap  ES3510MA-MIB
+            case 'SNMPv2-SMI::enterprises.259.8.1.11.2.1.0.200':
+            case 'SNMPv2-SMI::enterprises.259.8.1.11.2.1.0.208':
+            case 'SNMPv2-SMI::enterprises.259.8.1.11.2.1.0.107':
+            case 'SNMPv2-SMI::enterprises.259.8.1.11.2.1.0.108':
+            //               .1.3.6.1.4.1.259.6.10.94.2.1.0.1  swPowerStatusChangeTrap  ES3528MO-MIB
+            case 'SNMPv2-SMI::enterprises.259.6.10.94.2.1.0.67':
+            case 'SNMPv2-SMI::enterprises.259.6.10.94.2.1.0.36':
             case 'SNMPv2-SMI::enterprises.171.10.94.89.89.0.176':
+            case 'SNMPv2-MIB::authenticationFailure':
+            case 'SNMPv2-MIB::coldStart':
+            case 'SNMPv2-MIB::warmStart':
+            case 'SNMPv2-SMI::mib-2.17.0.2':
+            case 'iso.0.8802.1.1.2.0.0.1.0.1':
+            case 'iso.0.8802.1.1.2.0.0.1':                          //lldpRemTablesChange
                 break;
             default:
                 Log::debug("Unknown trap {$trap->oid} from {$trap->ip}");
